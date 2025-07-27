ccc58188ba91faf0d1e1dbbe9ec2a3bd78f7adb9
niki56780 <nicolynick026@gmail.com>
2025-07-27 15:19:17 -0300

    Create somar.py

---
+++ somar.py
@@ -0,0 +1,7 @@
+def somar(a , b):
+    return (a + b)
+
+numero = int(input("digite o primeiro numero: "))
+numero2= int(input("digite o segundo numero: "))
+resulte = numero + numero2
+print("esse é o valor: ",resulte)
